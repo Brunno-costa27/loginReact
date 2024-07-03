9d5d733183b0a86448c90f0580c919b18761eef9
Brunno Costa <sbrunno.costa@gmail.com>
2024-07-03 14:22:45 -0300

    :sparkles: feat: Add responsividade

--- src/components/Buttom.jsx
+++ src/components/Buttom.jsx
@@ -4,8 +4,8 @@ export function Buttom({ title, icon, color }) {
 
     return (
         <button className={`w-full flex items-center justify-center p-3 gap-2 bg-${color}-600 text-white rounded `}>
-            {icon && <img className="w-6 h-6" src={icon} alt="" />}
-            <p className="text-lg font-semibold">{title}</p>
+            {icon && <img className="w-4 h-4 sm:w-6 sm:h-6" src={icon} alt="" />}
+            <p className="text-base sm:text-lg font-semibold">{title}</p>
         </button>
     )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,26 +8,26 @@ export function Login() {
     return (
         <div className='w-full flex font-archivo'>
             {/* Imagem */}
-            <div className='w-full'>
+            <div className='hidden sm:flex sm:w-full'>
                 <img src={logo} alt="" />
             </div>
 
             {/* form */}
-            <div className='w-full flex flex-col justify-center items-center'>
-                <form action="" className='w-full flex flex-col p-28'>
+            <div className='w-full flex flex-col h-screen justify-center items-center'>
+                <form action="" className='w-full  flex flex-col p-10  sm:p-28'>
                     <div>
-                        <p className='text-base'>Bem-vindo de volta</p>
-                        <p className='text-xl font-bold'>Faça login na sua conta</p>
+                        <p className='text-sm sm:text-base'>Bem-vindo de volta</p>
+                        <p className='text-base sm:text-xl font-bold'>Faça login na sua conta</p>
                     </div>
 
                     <div className='flex flex-col mt-5'>
-                        <label className='py-2' htmlFor="">E-mail</label>
+                        <label className='py-2 text-sm sm:text-base' htmlFor="">E-mail</label>
                         <input
                             className='p-2 bg-[#F7FAFC] border border-[#E8E8E8] outline-none placeholder-[#656769]'
                             type="text"
                             placeholder='examplo@gmail.com'
                         />
-                        <label className='py-1' htmlFor="">Senha</label>
+                        <label className='py-1 text-sm sm:text-base' htmlFor="">Senha</label>
                         <input
                             type="password"
                             placeholder='01234567...'
@@ -38,11 +38,11 @@ export function Login() {
                     <div className='flex justify-between items-center py-2'>
                         <div className='flex gap-2'>
                             <input type="radio" />
-                            <p className='text-[#1A202C]'>Lembre de mim</p>
+                            <p className='text-[#1A202C] text-sm sm:text-base'>Lembre de mim</p>
                         </div>
 
                         <div>
-                            <a className='text-[#1E9E6A]' href="#">Esqueceu sua senha?</a>
+                            <a className='text-[#1E9E6A] text-sm sm:text-base' href="#">Esqueceu sua senha?</a>
                         </div>
                     </div>
 
@@ -63,11 +63,11 @@ export function Login() {
                                 color='stone'
                             />
                             </div>
-                        </div>
+                        </div>  
 
                         <div className='flex justify-center mt-10 gap-1'>
-                            <p className='text-lg'>Não tem uma conta?</p>
-                            <a className='text-lg text-green-600' href="">Cadastra-se</a>
+                            <p className='text-sm sm:text-lg'>Não tem uma conta?</p>
+                            <a className='text-sm sm:text-lg text-green-600' href="">Cadastra-se</a>
                         </div>
                     </div>
                 </form>
